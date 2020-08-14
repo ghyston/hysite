1d1f686c15596d020455ff03fed979802f6380ab
Hyston <ghyston@gmail.com>
2020-08-14 10:53:34 +0200

    Add Azure KeyVault to load github credentials

--- Hysite.Web/Program.cs
+++ Hysite.Web/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 
 namespace hySite
 {
@@ -19,7 +22,21 @@ namespace hySite
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((ctx, builder) => {
+
+                    //this code was taken from https://www.youtube.com/watch?v=k2VYcYS3EIA&t=846s
+                    var keyVaultEndpoint = GetKeyVaultEndpoint();
+                    if( string.IsNullOrWhiteSpace(keyVaultEndpoint))
+                        return;
+
+                    var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                    var authCallback = new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback);
+                    var keyVaultClient = new KeyVaultClient(authCallback);
+                    builder.AddAzureKeyVault(keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+                })
                 .UseStartup<Startup>()
                 .Build();
+
+        private static string GetKeyVaultEndpoint() => "https://kv-hysite.vault.azure.net/";
     }
 }

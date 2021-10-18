e372d9fd732ec76f7c627891523526fd301a0eb3
Ilja Stepanow <ghyston@gmail.com>
2021-10-18 21:46:55 +0200

    Convert methods to expression methods

--- Hysite.Web/Repository/Implementation/BlogPostRepository.cs
+++ Hysite.Web/Repository/Implementation/BlogPostRepository.cs
@@ -13,72 +13,48 @@ namespace hySite
             _dbContext = db;
         }
 
-        public void Add(BlogPost post)
-        {
-            _dbContext.Add(post);
-        }
+        public void Add(BlogPost post) => _dbContext.Add(post);
 
-        public void Add(IEnumerable<BlogPost> posts)
-        {
+        public void Add(IEnumerable<BlogPost> posts) => 
             _dbContext.AddRange(posts);
-        }
-            
-        public BlogPost FindPostByFileName(string fileName)
-        {
-            return _dbContext
+
+        public BlogPost FindPostByFileName(string fileName) => 
+            _dbContext
                 .BlogPosts
                 .Where(p => p.FileName == fileName)
                 .FirstOrDefault();
-        }
 
-        public IEnumerable<BlogPost> FindPostsByPage(int pageNumber, int postPerPage)
-        {
-            return _dbContext
+        public IEnumerable<BlogPost> FindPostsByPage(int pageNumber, int postPerPage) => 
+            _dbContext
                 .BlogPosts
                 .OrderByDescending(p => p.Created)
                 .Skip(pageNumber * postPerPage)
                 .Take(postPerPage).ToList();
 
-        }
-
-        //@todo: convert all methods here to expression methods in separate commit
         public IEnumerable<BlogPost> RetrieveAll() =>
              _dbContext
              .BlogPosts
              .OrderByDescending(p => p.Created);
 
-        public int PostsCount()
-        {
-            return _dbContext.BlogPosts.Count();
-        }
+        public int PostsCount() => _dbContext.BlogPosts.Count();
 
-        public void Remove(BlogPost post)
-        {
-            _dbContext.BlogPosts.Remove(post);
-        }
+        public void Remove(BlogPost post) => _dbContext.BlogPosts.Remove(post);
 
-        public void RemoveAll()
-        {
+        public void RemoveAll() =>
             _dbContext.BlogPosts.RemoveRange(_dbContext.BlogPosts);
-        }
 
-        public string FindNextPostFileName(DateTime time)
-        {
-            return _dbContext.BlogPosts
+        public string FindNextPostFileName(DateTime time) =>
+            _dbContext.BlogPosts
                 .Where(bp => bp.Created > time)
                 .OrderBy(bp => bp.Created)
                 .Select(bp => bp.FileName)
                 .FirstOrDefault();
-        }
 
-        public string FindPrevPostFileName(DateTime time)
-        {
-            return _dbContext.BlogPosts
+        public string FindPrevPostFileName(DateTime time) =>
+            _dbContext.BlogPosts
                 .Where(bp => bp.Created < time)
                 .OrderByDescending(bp => bp.Created)
                 .Select(bp => bp.FileName)
                 .FirstOrDefault();
-
-        }
     }
 }

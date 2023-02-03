6e817c6dfff51b11afec61b08b73043348da4288
Ilja Stepanow <ghyston@gmail.com>
2023-02-03 17:18:15 +0100

    Run detached; Check db connection string in env

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -4,14 +4,21 @@ using HySite.Application.Interfaces;
 using HySite.Infrastructure.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace HySite.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration, ILogger logger)
     {
-        var connectionString = configuration.GetConnectionString("Database");
+        var envConnectionStr = Environment.GetEnvironmentVariable("HYSITE_DB_CONNECTION");
+        var connectionString = string.IsNullOrWhiteSpace(envConnectionStr) 
+            ? configuration.GetConnectionString("Database")
+            : envConnectionStr;
+
+        logger.LogInformation($"Try to connect db {connectionString}");
+        
         services.AddDbContext<AppDbContext>(options => options
             .UseNpgsql(connectionString)
             .UseSnakeCaseNamingConvention());
